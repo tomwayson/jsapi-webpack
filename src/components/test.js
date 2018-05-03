912e713faa80adb0908644e67da2e3a348ea1560
Tom Wayson <tom@tomwayson.com>
2018-05-03 16:32:33 -0700

    WIP: install react-loadable and try to lazy load the JSAPI bundle

---
+++ src/components/test.js
@@ -0,0 +1,5 @@
+import React  from 'react';
+
+export default () => {
+  return <p>Map goes here.</p>;
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import "@dojo/shim/Promise";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Loadable from 'react-loadable';
+
 import { Header } from "./components/header";
-import { WebMapComponent } from "./components/webmapview";
 
 import "./css/main.scss";
+
+const LoadableWebMapComponent = Loadable({
+  // NOTE: this works:
+  // loader: () => import("./components/test"),
+  // but this:
+  loader: () => import("./components/webmapview"),
+  // causes the following runtime error:
+  // Warning: React.createElement: type is invalid -- 
+  // expected a string (for built-in components) or a class/function (for composite components) but got: undefined.
+  // You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.
+  loading() {
+    return <div>Loading...</div>
+  }
+});
 
 const addDOMNode = () => {
   const appNode = document.createElement("div");
@@ -21,7 +36,7 @@ const addDOMNode = () => {
 ReactDOM.render(
   <div className="main">
     <Header appName="Webpack App"/>
-    <WebMapComponent />
+    <LoadableWebMapComponent />
   </div>,
   addDOMNode()
 );
