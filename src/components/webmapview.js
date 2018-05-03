6ba29d6791d21861d8a790810b1607c92e993e5b
Tom Wayson <tom@tomwayson.com>
2018-05-03 15:45:16 -0700

    consolidate map code into webmapview component

--- src/components/webmapview.js
+++ src/components/webmapview.js
@@ -1,14 +1,35 @@
+import "../config";
+
+import FeatureLayer from "esri/layers/FeatureLayer";
+import WebMap from "esri/WebMap";
 import SceneView from "esri/views/SceneView";
 import React from "react";
 
 export class WebMapComponent extends React.Component {
 
   componentDidMount() {
+    const featureLayer = new FeatureLayer({
+      id: "states",
+      portalItem: {
+        id: "b234a118ab6b4c91908a1cf677941702"
+      },
+      outFields: ["NAME", "STATE_NAME", "VACANT", "HSE_UNITS"],
+      title: "U.S. counties"
+    });
+    
+    const webmap = new WebMap({
+      portalItem: {
+        id: "3ff64504498c4e9581a7a754412b6a9e"
+      },
+      layers: [featureLayer]
+    });
     const view = new SceneView({
-      map: this.props.webmap,
+      map: webmap,
       container: this.mapDiv
     });
-    this.props.onload(view);
+    featureLayer.when(() => {
+      view.goTo({ target: featureLayer.fullExtent });
+    });
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import "@dojo/shim/Promise";
-import "./config";
-
-import FeatureLayer from "esri/layers/FeatureLayer";
-import WebMap from "esri/WebMap";
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -19,37 +15,13 @@ const addDOMNode = () => {
   return appNode;
 }
 
-const onComponentLoad = (view) => {
-  featureLayer.when(() => {
-    view.goTo({ target: featureLayer.fullExtent });
-  });
-};
-
-const featureLayer = new FeatureLayer({
-  id: "states",
-  portalItem: {
-    id: "b234a118ab6b4c91908a1cf677941702"
-  },
-  outFields: ["NAME", "STATE_NAME", "VACANT", "HSE_UNITS"],
-  title: "U.S. counties"
-});
-
-const webmap = new WebMap({
-  portalItem: {
-    id: "3ff64504498c4e9581a7a754412b6a9e"
-  },
-  layers: [featureLayer]
-});
-
 /**
  * React portion of application
  */
 ReactDOM.render(
   <div className="main">
     <Header appName="Webpack App"/>
-    <WebMapComponent
-      webmap={webmap}
-      onload={onComponentLoad} />
+    <WebMapComponent />
   </div>,
   addDOMNode()
 );
